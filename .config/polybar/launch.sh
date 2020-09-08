be1d5ed00126fc23d1d16ee80c76b0177599acbb
ecosse3 <theecosse@gmail.com>
2020-09-08 23:50:15 +0200

    feat(polybar): enable opacity and rounded corners depending on compton/picom process

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,10 +6,20 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
-  done
+if pgrep picom || pgrep compton; then
+    if type "xrandr"; then
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload main &
+      done
+    else
+      polybar --reload example &
+    fi
 else
-  polybar --reload example &
+    if type "xrandr"; then
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload main-no-compton &
+      done
+    else
+      polybar --reload example &
+    fi
 fi
