5103f7d338f0def6d674bfb2f8199c5050eb6019
ecosse3 <luk.kurpiewski@gmail.com>
2022-01-18 02:29:51 +0100

    awesome: modify icons

--- .config/awesome/widget/search-apps/init.lua
+++ .config/awesome/widget/search-apps/init.lua
@@ -13,7 +13,7 @@ local return_button = function()
 	local widget = wibox.widget {
 		{
 			id = 'icon',
-			image = widget_icon_dir .. 'app-launcher.svg',
+			image = widget_icon_dir .. 'menu-arch.svg',
 			widget = wibox.widget.imagebox,
 			resize = true
 		},
