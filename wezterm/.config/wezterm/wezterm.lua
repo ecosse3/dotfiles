7adb1023bc7fcd2d46584a4f38b5217eedca3006
ecosse3 <luk.kurpiewski@gmail.com>
2024-09-29 10:20:47 +0200

    fix(wezterm): adjust font size for better readability

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -114,9 +114,11 @@ config.window_padding = {
 config.color_scheme = "Tokyo Night (Gogh)"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.harfbuzz_features = { "zero", "cv05", "cv02", "ss05", "ss04" }
-config.font_size = 18.0
+config.font_size = 17.0
 config.enable_scroll_bar = true
-config.window_background_opacity = 0.90
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 20
 
 config.launch_menu = {}
 
